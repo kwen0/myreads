36758ebf4b5ec4a38619e0bbe6f7982207235ac3
Karen <karen.wen3@gmail.com>
2022-02-10 15:48:55 -0500

    added logo, font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import styled from 'styled-components';
+import logo from './logo.png';
+import "@fontsource/merriweather";
 
 function App() {
-  return (
-    <div>
 
-    </div>
+  return (
+    <p>
+      <img src={logo} className="logo" alt="logo" />
+      Helo world
+    </p>
   );
 }
 
